d9d7c97c74bcbc4f04cde350d81c445a42e51abe
albseb511 <albertsebastian511@gmail.com>
2020-04-10 22:54:52 +0530

    Update: routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from './Router/Router'
+
+const arr = [
+  {
+    id: 500,
+    name: "Shoes"
+  },
+  {
+    id: 300,
+    name: "Pants"
+  }
+]
+
+const Home = () => <div>Home</div>
+
+const About = () => <div>About</div>
+
+const Contact = () => <div>Contact </div>
+
+const Product = ({match}) => {
+  return (
+  <div>
+    <Link to={`${match.url}/500`}>Shoes</Link>
+    <br/>
+    <Link to={`${match.url}/300`}>Pants</Link>
+    <br/>
+  </div>
+  )
+}
+const Products = ({match}) => {
+  let item = arr.find(a=>a.id==match.params.id)
+  console.log(item,match)
+  if(!item){
+    return null
+  }
+  return (
+  <div>
+    <div>ID: {item.id}</div>
+    <div>NAME: {item.name}</div>
+  </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Navbar
+        <br/>
+        <Link to='/'>Home</Link>
+        <br/>
+        <Link to='/about'>About</Link>
+        <br/>
+        <Link to='/contact'>Contact</Link>
+        <br/>
+        <Link to='/products'>Products</Link>
+      </div>
+      <div>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' render={()=><About/>} />
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+        <Route path='/products' render={(props)=><Product {...props}/>} /> 
+        <Route path='/products/:id' render={(props)=><Products {...props}/>} /> 
+      </div>
     </div>
   );
 }
